82f4af1fec2d25207592a1c69d841aa056d08a09
Christian Cordero <christiancordero092@gmail.com>
2021-07-01 15:06:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif !!phrase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.downcase
+  else 
+    return "NO, NOT SINCE 1938!"
   end
 end
